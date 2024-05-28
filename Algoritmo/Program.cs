9c08955048ebf119b61f1459537b52449bb74371
Alka <ajiju.2000@hotmail.com>
2024-05-28 16:19:58 -0600

    se cambio la posicion de fechasxturno

--- Algoritmo/Program.cs
+++ Algoritmo/Program.cs
@@ -5,12 +5,13 @@ ScedbContext context = new();
 AlgoritmoPro algoritmo = new(context);
 
 var gruposPorCalendarizar =algoritmo.GetGrupoPeriodo(2203);
+var FechasXTurnos = algoritmo.FechasByTurno(2203);
 
 foreach (var grupo in gruposPorCalendarizar.OrderBy(x=>x.Value.Count()))
 {
 	foreach (var materia in grupo.Value)
 	{
-		var FechasXTurnos = algoritmo.FechasByTurno(2203);
+		
 		if(materia.Bloque == "Matutino")
 		{
 
